382a3ad9878fbaedef61b40d21e646d81dfc5f22
Ben Cottier <bcotter2@gmail.com>
2023-05-29 12:54:55 +0100

    Add get_institution_counts

--- researcher_impact/processors.py
+++ researcher_impact/processors.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from pyalex import Concepts, Works
 from researcher_impact.pyalex_utils import merge_pages, merge_sample
+from researcher_impact.citations import get_citation_count_in_first_years
 
 class OpenAlexProcessor:
     def __init__(self):
@@ -26,6 +27,24 @@ class OpenAlexProcessor:
                         ins_name = ins['display_name']
                         named_institution_author_data[ins_name][pub_year].add(author_name)
         return institution_author_data, named_institution_author_data
+    
+    @classmethod
+    def get_institution_counts(cls, works, selected_institution_ids=None):
+        institution_work_counts = defaultdict(lambda: defaultdict(int))
+        institution_citation_counts = defaultdict(lambda: defaultdict(int))
+        for work in works:
+            pub_year = work['publication_year']
+            citation_count = get_citation_count_in_first_years(work)
+            for authorship in work['authorships']:
+                if len(authorship['institutions']) > 0:
+                    for ins in authorship['institutions']:
+                        if ins.get('id') is None:
+                            continue
+                        if selected_institution_ids is not None and ins['id'] not in selected_institution_ids:
+                            continue
+                        institution_citation_counts[ins['id']][pub_year] += citation_count
+                        institution_work_counts[ins['id']][pub_year] += 1
+        return institution_citation_counts, institution_work_counts
 
     @classmethod
     def get_institution_works(cls, institution_id, concept_ids=None):
