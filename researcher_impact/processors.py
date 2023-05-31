eedacd3dbf26e1e56c25fb086412997f1f403e5a
Ben Cottier <bcotter2@gmail.com>
2023-05-31 17:12:53 +0100

    fix: handle missing author id

--- researcher_impact/processors.py
+++ researcher_impact/processors.py
@@ -15,6 +15,8 @@ class OpenAlexProcessor:
             pub_year = work['publication_year']
             for authorship in work['authorships']:
                 if len(authorship['institutions']) > 0:
+                    if authorship['author'].get('id') is None:
+                        continue
                     author_id = authorship['author']['id']
                     author_name = authorship['author']['display_name']
                     for ins in authorship['institutions']:
