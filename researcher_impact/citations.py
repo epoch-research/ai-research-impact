fc22670d3c45ac1b55eab2c988785fb4d5e99b62
Ben Cottier <bcotter2@gmail.com>
2023-06-16 12:20:17 +0100

    Fix: citations from before publication year

--- researcher_impact/citations.py
+++ researcher_impact/citations.py
@@ -5,7 +5,7 @@ def get_citation_count_in_first_years(work, years=3):
     counts_by_year = work['counts_by_year']
     total_count = 0
     for year_count in counts_by_year:
-        if year_count['year'] <= pub_year + years:
+        if year_count['year'] >= pub_year and year_count['year'] <= pub_year + years:
             total_count += year_count['cited_by_count']
     return total_count
 
